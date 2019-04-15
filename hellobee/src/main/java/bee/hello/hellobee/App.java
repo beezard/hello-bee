52012ae187cd76c59b1f4f4dbc8efd72f519dbdd
user <user@BeezDesktop>
2019-04-15 22:11:48 +0900

    My first git project - hellobee

---
+++ hellobee/src/main/java/bee/hello/hellobee/App.java
@@ -0,0 +1,13 @@
+package bee.hello.hellobee;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello Bee!" );
+    }
+}
